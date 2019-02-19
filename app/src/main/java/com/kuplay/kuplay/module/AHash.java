5b6680de61b256175b7f33e27449becf4ff44a20
yineng <yineng@tom.com>
2019-02-19 17:34:01 +0800

    12333

--- app/src/main/java/com/kuplay/kuplay/module/AHash.java
+++
@@ -1,9 +0,0 @@
-package com.kuplay.kuplay.module;
-
-public class AHash {
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-    public static native String ahash(int[] pixels, int width, int height, int chanels);
-}
